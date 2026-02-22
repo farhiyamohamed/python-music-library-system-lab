cd3b5c02638b03b8412ff0ca1232d5f977e0e409
farhiyamohamed <farhiyam518@gmail.com>
2026-02-22 21:40:46 +0300

    Final updates: add comments to Song class and update README

--- lib/song.py
+++ lib/song.py
@@ -1,10 +1,10 @@
 class Song:
-    # Class attributes
-    count = 0
-    genres = []
-    artists = []
-    genre_count = {}
-    artists_count = {}
+    # Class attributes to track global library info
+    count = 0            # Total number of songs created
+    genres = []          # List of all unique genres
+    artists = []         # List of all unique artists
+    genre_count = {}     # Dictionary of song counts per genre
+    artists_count = {}   # Dictionary of song counts per artist
 
     def __init__(self, name, artist, genre):
         # Instance attributes
@@ -12,7 +12,7 @@ class Song:
         self.artist = artist
         self.genre = genre
 
-        # Update class-level info
+        # Update class-level info whenever a new song is created
         self.add_song_to_count()
         self.add_to_genres()
         self.add_to_artists()
@@ -23,35 +23,35 @@ class Song:
     def add_song_to_count(self):
         Song.count += 1
 
-    # Add genre if unique
+    # Add genre to unique genres list
     def add_to_genres(self):
         if self.genre not in Song.genres:
             Song.genres.append(self.genre)
 
-    # Add artist if unique
+    # Add artist to unique artists list
     def add_to_artists(self):
         if self.artist not in Song.artists:
             Song.artists.append(self.artist)
 
-    # Update count per genre
+    # Update count of songs per genre
     def add_to_genre_count(self):
         if self.genre in Song.genre_count:
             Song.genre_count[self.genre] += 1
         else:
             Song.genre_count[self.genre] = 1
 
-    # Update count per artist
+    # Update count of songs per artist
     def add_to_artists_count(self):
         if self.artist in Song.artists_count:
             Song.artists_count[self.artist] += 1
         else:
             Song.artists_count[self.artist] = 1
 
-    # Display info for a single song
+    # Display info for this song
     def display_info(self):
         print(f"Song: {self.name}, Artist: {self.artist}, Genre: {self.genre}")
 
-    # Display library-wide info
+    # Display global library info
     @classmethod
     def display_library_info(cls):
         print(f"Total Songs: {cls.count}")
